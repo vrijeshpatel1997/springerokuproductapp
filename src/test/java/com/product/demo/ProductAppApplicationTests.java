c0da417b0253d8510fdc5f5023872f83494c4d8e
Vrijesh Patel <60836824+vrijeshpatel1997@users.noreply.github.com>
2021-05-18 08:16:48 -0700

    Add files via upload

---
+++ src/test/java/com/product/demo/ProductAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
